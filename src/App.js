590aca9fe830ff053da96f0e3c1f545887faf8ec
maydin52 <aydin.mehmet@msn.com>
2024-06-26 18:01:33 +0300

    Ok

--- src/App.js
+++ src/App.js
@@ -23,5 +23,11 @@ export default function App() {
              
         4. Bu görevleri doğru bir şekilde tamamlarsanız, butona tıkladığınızda bazı güzel görsel efektlere sahip olmalı ve çalışmamızın arka planı otomatik olarak açık mavi bir arka planla değiştirilmelidir.
 */
-  return <div></div>;
+  const handleGameRunning = () => {
+    setGameRunning(!gameRunning);
+  }
+
+  return <div>
+    <button className="video-game-button" onClick={handleGameRunning}>{gameRunning ? "Pause" : "Play"}</button>
+  </div>;
 }
